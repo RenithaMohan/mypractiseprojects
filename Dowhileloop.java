283b25d9018a45885cee17ecafc30c890a42933b
RenithaMohan <154872161+RenithaMohan@users.noreply.github.com>
2023-12-28 21:49:14 +0530

    Add files via upload

---
+++ Dowhileloop.java
@@ -0,0 +1,19 @@
+package whileloop;
+
+public class Dowhileloop {
+
+	public static void main(String[] args) 
+		
+	{  
+	   int i=1;  
+	   do
+	{  
+		    System.out.println(i);  
+		    i++;  
+		    }
+	while(i<=10);  
+
+		// TODO Auto-generated method stub
+
+	}
+	}
