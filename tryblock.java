fc67fae81fde9131b0dc2ffde947800e9116f118
RenithaMohan <154872161+RenithaMohan@users.noreply.github.com>
2023-12-29 15:42:13 +0530

    Add files via upload

---
+++ tryblock.java
@@ -0,0 +1,22 @@
+package trycatch;
+import java.io.FileOutputStream;
+public class tryblock{
+
+	public static void main(String[] args) {
+		try(FileOutputStream fileOutputStream =new FileOutputStream("abc.txt")){      
+			String msg = "Welcome to java!";      
+			byte byteArray[] = msg.getBytes(); //converting string into byte array      
+			fileOutputStream.write(byteArray);  
+System.out.println("Message written to file successfuly!");      
+			}catch(Exception exception){  
+			       System.out.println(exception);  
+			}
+	}
+}
+
+
+		// TODO Auto-generated method stub
+
+	         
+
+
