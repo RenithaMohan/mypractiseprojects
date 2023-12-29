e268af9e431e37a3a256a898dfb985d78e1720a3
RenithaMohan <154872161+RenithaMohan@users.noreply.github.com>
2023-12-29 15:48:44 +0530

    Add files via upload

---
+++ multiplecatch/muticatch1.java
@@ -0,0 +1,26 @@
+package multiplecatch;
+
+public class muticatch1 {
+
+	public static void main(String[] args) {
+		try{
+            int a[]=new int[5];
+            System.out.println(a[10]);
+        }
+        catch(ArithmeticException e){
+            System.out.println("Arithmetic exception");
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+           System.out.println("ArrayIndexOutOfBounds exception");
+        }
+        catch(Exception e){
+            System.out.println("Parent exception");
+        }
+        System.out.println("rest of the code");
+    }
+
+		// TODO Auto-generated method stub
+
+	}
+
+
